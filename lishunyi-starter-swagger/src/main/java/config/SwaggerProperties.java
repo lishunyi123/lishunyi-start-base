f1b0a8c89fad7a2a07378b80520a9cbc9e969b3d
李顺仪 <lishunyi0109@gmail.com>
2019-10-12 18:36:01 +0800

    [ADD] 增加swagger依赖及配置

---
+++ lishunyi-starter-swagger/src/main/java/config/SwaggerProperties.java
@@ -0,0 +1,26 @@
+package config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @ClassName SwaggerProperties
+ * @Description java类作用描述
+ * @Author 李顺仪
+ * @CreateDate 2019/10/12 17:44
+ * @UpdateUser 李顺仪
+ * @UpdateDate 2019/10/12 17:44
+ * @UpdateRemark 修改内容
+ * @Version 1.0
+ **/
+@Data
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+
+    private String title;
+    private String description;
+    private String termsOfServiceUrl;
+    private String contact;
+    private String version = "v1.0";
+    private String basePackage;
+}
