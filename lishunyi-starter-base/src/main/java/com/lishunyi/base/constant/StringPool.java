9d27e81e6272f866139f1c892d4a14c99c043265
李顺仪 <lishunyi0109@gmail.com>
2019-12-13 18:14:23 +0800

    :sparkles: 增加字符串工具类

---
+++ lishunyi-starter-base/src/main/java/com/lishunyi/base/constant/StringPool.java
@@ -0,0 +1,69 @@
+package com.lishunyi.base.constant;
+
+/**
+ * @ClassName StringPool
+ * @Description String常量池
+ * @Author 李顺仪
+ * @CreateDate 2019/12/13 16:20
+ * @UpdateUser 李顺仪
+ * @UpdateDate 2019/12/13 16:20
+ * @UpdateRemark 修改内容
+ * @Version 1.0
+ **/
+public class StringPool {
+
+	public static final String AMPERSAND = "&";
+	public static final String AND = "and";
+	public static final String AT = "@";
+	public static final String ASTERISK = "*";
+	public static final String STAR = ASTERISK;
+	public static final String BACK_SLASH = "\\";
+	public static final String COLON = ":";
+	public static final String COMMA = ",";
+	public static final String DASH = "-";
+	public static final String DOLLAR = "$";
+	public static final String DOT = ".";
+	public static final String EMPTY = "";
+	public static final String EQUALS = "=";
+	public static final String FALSE = "false";
+	public static final String SLASH = "/";
+	public static final String HASH = "#";
+	public static final String HAT = "^";
+	public static final String LEFT_BRACE = "{";
+	public static final String LEFT_BRACKET = "(";
+	public static final String LEFT_CHEV = "<";
+	public static final String NEWLINE = "\n";
+	public static final String N = "n";
+	public static final String NO = "no";
+	public static final String NULL = "null";
+	public static final String OFF = "off";
+	public static final String ON = "on";
+	public static final String PERCENT = "%";
+	public static final String PIPE = "|";
+	public static final String PLUS = "+";
+	public static final String QUESTION_MARK = "?";
+	public static final String EXCLAMATION_MARK = "!";
+	public static final String QUOTE = "\"";
+	public static final String RETURN = "\r";
+	public static final String TAB = "\t";
+	public static final String RIGHT_BRACE = "}";
+	public static final String RIGHT_BRACKET = ")";
+	public static final String RIGHT_CHEV = ">";
+	public static final String SEMICOLON = ";";
+	public static final String SINGLE_QUOTE = "'";
+	public static final String BACKTICK = "`";
+	public static final String SPACE = " ";
+	public static final String TILDA = "~";
+	public static final String LEFT_SQ_BRACKET = "[";
+	public static final String RIGHT_SQ_BRACKET = "]";
+	public static final String TRUE = "true";
+	public static final String UNDERSCORE = "_";
+	public static final String UTF_8 = "UTF-8";
+	public static final String GBK = "GBK";
+	public static final String Y = "y";
+	public static final String YES = "yes";
+	public static final String ONE = "1";
+	public static final String ZERO = "0";
+	public static final String DOLLAR_LEFT_BRACE = "${";
+	public static final String UNKNOWN = "unknown";
+}
---
+++ lishunyi-starter-base/src/main/java/com/lishunyi/base/utils/StringUtil.java
@@ -0,0 +1,195 @@
+package com.lishunyi.base.utils;
+
+import com.lishunyi.base.constant.CharPool;
+import com.lishunyi.base.constant.StringPool;
+import lombok.experimental.UtilityClass;
+import org.springframework.lang.Nullable;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * @ClassName StringUtil
+ * @Description java类作用描述
+ * @Author 李顺仪
+ * @CreateDate 2019/12/13 10:33
+ * @UpdateUser 李顺仪
+ * @UpdateDate 2019/12/13 10:33
+ * @UpdateRemark 修改内容
+ * @Version 1.0
+ **/
+@UtilityClass
+public class StringUtil extends StringUtils {
+
+	/**
+	 * 特殊字符正则，sql特殊字符和空白符.
+	 */
+	private static final Pattern SPECIAL_CHARS_REGEX = Pattern.compile("[`'\"|/,;()-+*%#·•�　\\s]");
+
+	/***
+	 * 随机数字符串因子
+	 */
+	private static final String INT_STR = "0123456789";
+	private static final String STR_STR = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private static final String ALL_STR = INT_STR + STR_STR;
+
+	/**
+	 * 首字符转小写
+	 *
+	 * @param str {@code String} 字符串
+	 * @return {@code String}
+	 */
+	public static String firstCharToLower(String str) {
+		char firstChar = str.charAt(0);
+		if (firstChar >= CharPool.UPPER_A && firstChar <= CharPool.UPPER_Z) {
+			char[] arr = str.toCharArray();
+			arr[0] += (CharPool.LOWER_A - CharPool.LOWER_Z);
+			return new String(arr);
+		}
+		return str;
+	}
+
+	/**
+	 * 首字符转大写
+	 *
+	 * @param str {@code String} 字符串
+	 * @return {@code String}
+	 */
+	public static String firstCharToUpper(String str) {
+		char firstChar = str.charAt(0);
+		if (firstChar >= CharPool.LOWER_A && firstChar <= CharPool.LOWER_Z) {
+			char[] arr = str.toCharArray();
+			arr[0] -= (CharPool.LOWER_A - CharPool.LOWER_Z);
+			return new String(arr);
+		}
+		return str;
+	}
+
+	/**
+	 * 是否为空
+	 *
+	 * @param cs {@code CharSequence} 字符串
+	 * @return {@code boolean}
+	 */
+	public static boolean isBlank(@Nullable final CharSequence cs) {
+		return !StringUtil.hasLength(cs);
+	}
+
+	/**
+	 * 是否不为空
+	 *
+	 * @param cs {@code CharSequence} 字符串
+	 * @return {@code boolean}
+	 */
+	public static boolean isNotBlank(@Nullable final CharSequence cs) {
+		return StringUtil.hasLength(cs);
+	}
+
+	/**
+	 * 有任意一个为空
+	 *
+	 * @param css {@code CharSequence} 字符串列
+	 * @return {@code boolean}
+	 */
+	public static boolean isAnyBlank(final CharSequence... css) {
+		if (ObjectUtils.isEmpty(css)) {
+			return true;
+		}
+		return Stream.of(css).anyMatch(StringUtil::isBlank);
+	}
+
+	/**
+	 * 是否全部不是空
+	 *
+	 * @param css {@code CharSequence} 字符串列
+	 * @return {@code boolean}
+	 */
+	public static boolean isNoneBlank(final CharSequence css) {
+		if (ObjectUtils.isEmpty(css)) {
+			return false;
+		}
+		return Stream.of(css).allMatch(StringUtil::isNoneBlank);
+	}
+
+	/**
+	 * 判断字符串是否为数字
+	 *
+	 * @param cs {@code CharSequence} 字符串
+	 * @return {@code boolean}
+	 */
+	public static boolean isNumeric(final CharSequence cs) {
+		if (StringUtil.isBlank(cs)) {
+			return false;
+		}
+		for (int i = cs.length(); --i >= 0; ) {
+			int chr = cs.charAt(i);
+			if (chr < 48 || chr > 57) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 将字符串中特定模式的字符替换成map对应key的值
+	 *
+	 * @param message {@code String} 转换前的字符串
+	 * @param params  {@code Map<String, Object>} 转换所需的键值对
+	 * @return {@code String} 转换后的字符串
+	 */
+	public static String format(@Nullable String message, @Nullable Map<String, Object> params) {
+		// 如果message为空，返回“”
+		if (StringUtil.isBlank(message)) {
+			return StringPool.EMPTY;
+		}
+		// 如果参数为空，返回消息
+		if (ObjectUtils.isEmpty(params)) {
+			return message;
+		}
+		// 替换变量
+		StringBuilder sb = new StringBuilder((int) (message.length() * 1.5));
+		int cursor = 0;
+		for (int start, end; (start = message.indexOf(StringPool.DOLLAR_LEFT_BRACE, cursor)) != -1 && (end = message.indexOf(CharPool.RIGHT_BRACE, start)) != -1; ) {
+			sb.append(message, cursor, start);
+			String key = message.substring(start + 2, end);
+			Object value = params.get(StringUtil.trimWhitespace(key));
+			sb.append(value == null ? StringPool.EMPTY : value);
+			cursor = end + 1;
+		}
+		sb.append(message.substring(cursor));
+		return sb.toString();
+	}
+
+	/**
+	 * 同上，使用log模式
+	 *
+	 * @param message   {@code String} 转换前的字符串
+	 * @param arguments {@code Object} 替换的变量
+	 * @return {@code String} 转换后的字符串
+	 */
+	public static String format(@Nullable String message, @Nullable Object... arguments) {
+		// 如果message为空，返回“”
+		if (StringUtil.isBlank(message)) {
+			return StringPool.EMPTY;
+		}
+		// 如果参数为空，返回消息
+		if (ObjectUtils.isEmpty(arguments)) {
+			return message;
+		}
+		StringBuilder sb = new StringBuilder((int) (message.length() * 1.5));
+		int cursor = 0;
+		int index = 0;
+		int argsLength = arguments.length;
+		for (int start, end; (start = message.indexOf(CharPool.LEFT_BRACE, cursor)) != -1 && (end = message.indexOf(CharPool.RIGHT_BRACE, start)) != -1 && index < argsLength; ) {
+			sb.append(message, cursor, start);
+			sb.append(arguments[index]);
+			cursor = end + 1;
+			index++;
+		}
+		sb.append(message.substring(cursor));
+		return sb.toString();
+	}
+}
