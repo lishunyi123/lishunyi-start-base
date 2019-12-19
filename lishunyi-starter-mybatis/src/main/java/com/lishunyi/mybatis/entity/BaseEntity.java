0c24fa1e9672cbc221ddbc42aa32f10d858b0f4b
李顺仪 <lishunyi0109@gmail.com>
2019-12-19 09:52:37 +0800

    :sparkles: 增加逻辑删除注解

--- lishunyi-starter-mybatis/src/main/java/com/lishunyi/mybatis/entity/BaseEntity.java
+++ lishunyi-starter-mybatis/src/main/java/com/lishunyi/mybatis/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.lishunyi.mybatis.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -60,6 +57,7 @@ public abstract class BaseEntity implements Serializable {
 	 * 是否删除：1表示删除，0表示未删除
 	 */
 	@TableField("is_deleted")
+	@TableLogic
 	private Boolean deleted;
 
 	/**
