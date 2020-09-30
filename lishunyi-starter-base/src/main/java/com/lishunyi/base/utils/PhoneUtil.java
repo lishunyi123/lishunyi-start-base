42f145e8b5d58a9f3ad5b26448645cded5472847
李顺仪 <lishunyi0109@gmail.com>
2020-09-30 17:27:41 +0800

    :sparkles: 增加手机正则

---
+++ lishunyi-starter-base/src/main/java/com/lishunyi/base/utils/PhoneUtil.java
@@ -0,0 +1,21 @@
+package com.lishunyi.base.utils;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author 李顺仪
+ * @version 1.0
+ * @since 2020/9/30 16:24
+ **/
+@UtilityClass
+public class PhoneUtil {
+
+	public static final String PHONE_PATTERN = "^1(3([0-35-9]\\d|4[1-8])|4[14-9]\\d|5([0125689]\\d|7[1-79])|66\\d|7[2-35-8]\\d|8\\d{2}|9[13589]\\d)\\d{7}$";
+
+	public boolean checkPhone(String phone) {
+		Pattern pattern = Pattern.compile(PHONE_PATTERN);
+		return pattern.matcher(phone).matches();
+	}
+}
